0e49f9fb91ef85d74accadd3b6f14a3d1e3c5490
0chamoy0 <1.janek.lipinski@gmail.com>
2025-10-14 21:55:05 +0200

    added caching system

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,10 +1,40 @@
+enum dataType{
+  weatherData = 'weatherData',
+  AQIData = 'AQIData'
+}
+function generateCacheKey(lat: string, lon: string, dataType: dataType){
+  const location = `${lat},${lon}`
+  const date = new Date()
+  const timestamp = `${date.getDate()}/${date.getHours()}`
+  const key = `${dataType}_${location}_${timestamp}`
+  return key
+}
+function isInCache(key: string){
+  return localStorage.getItem(key) ? true : false
+}
+
 export async function getWeather(lat: string, lon: string) {
-  const res = await fetch(`/api/weatherData?lat=${lat}&lon=${lon}`);
-  return await res.json();
+  const cacheKey = generateCacheKey(lat, lon, dataType.weatherData)
+  if(!isInCache(cacheKey)){
+    const res = await fetch(`/api/weatherData?lat=${lat}&lon=${lon}`);
+    const data = await res.json()
+    localStorage.setItem(cacheKey, JSON.stringify(data))
+    return data
+  } else {
+    return localStorage.getItem(cacheKey)
+  }
+  
 }
 export async function getAQI(lat: string, lon: string) {
-  const res = await fetch(`/api/airQualityData?lat=${lat}&lon=${lon}`);
-  return await res.json();
+  const cacheKey = generateCacheKey(lat, lon, dataType.AQIData)
+  if(!isInCache(cacheKey)){
+    const res = await fetch(`/api/airQualityData?lat=${lat}&lon=${lon}`);
+    const data = await res.json()
+    localStorage.setItem(cacheKey, JSON.stringify(data))
+    return data
+  } else{
+    return localStorage.getItem(cacheKey)
+  }
 }
 type Coordinates = {
   lat: number;
